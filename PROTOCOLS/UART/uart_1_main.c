71d99909d42756c08c9f11418def59fdecc146e9
Ashwin <ashwinkumarh147@gmail.com>
2025-07-24 17:21:47 +0530

    add uart protocol

---
+++ PROTOCOLS/UART/uart_1_main.c
@@ -0,0 +1,18 @@
+#include<lpc21xx.h>
+#include"uart_driver.h"
+#include"lcd1.h"
+
+int main()
+{
+	uart_init();
+		lcd_init();
+	lcd_command(0X80);
+		lcd_command(0x01);
+	uart_str("Hello\r");
+	lcd_string("Hello");
+		lcd_command(0xC0);
+	lcd_string("transmitted");
+   while(1);
+		
+	}
+	
